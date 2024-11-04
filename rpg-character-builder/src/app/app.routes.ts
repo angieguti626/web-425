8f18664760ec07cfa8e230dc06b0511f3be0b4c3
Angelica Gutierrez <angelicagu368@gmail.com>
2024-11-03 19:00:32 -0800

    updating routes

--- rpg-character-builder/src/app/app.routes.ts
+++ rpg-character-builder/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 // add a route for the HomeComponent
 import { HomeComponent } from './home/home.component';
+// add new routes for 2.1
+import { PlayersComponent } from './players/players.component';
+import { SigninComponent } from './signin/signin.component';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { CreateGuildComponent } from './create-guild/create-guild.component';
+import { CharacterFactionComponent } from './character-faction/character-faction.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +16,25 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'players',
+    component: PlayersComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'create-character',
+    component: CreateCharacterComponent
+  },
+  {
+    path: 'create-guild',
+    component: CreateGuildComponent
+  },
+  {
+    path: 'character-faction',
+    component: CharacterFactionComponent
   }
 ];
