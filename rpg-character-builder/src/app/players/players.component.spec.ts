8fa4ab443e5eaa59a605453f9cdbd002d202ce27
Angelica Gutierrez <angelicagu368@gmail.com>
2024-11-09 15:27:13 -0800

    players

--- rpg-character-builder/src/app/players/players.component.spec.ts
+++ rpg-character-builder/src/app/players/players.component.spec.ts
@@ -17,7 +17,16 @@ describe('PlayersComponent', () => {
     fixture.detectChanges();
   });
 
+  // Created by default when the component is generated
+  // Unit test 1
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Unit test 2
+  it('should correctly display a list of players', () => {
+    const compiled = fixture.nativeElement as HTMLElement; // Get the compiled HTML of the component
+    const playersItems = compiled.querySelectorAll('.players-item'); // Get all the players items
+    expect(playersItems.length).toEqual(component.players.length); // Check if the number of players items is equal to the number of items in the players array
   });
 });
