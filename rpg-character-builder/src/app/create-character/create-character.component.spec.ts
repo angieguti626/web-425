e0678e2f8c38a1d85942f48921250732d969ceca
Angelica Gutierrez <angelicagu368@gmail.com>
2024-11-17 13:47:08 -0800

    create character

--- rpg-character-builder/src/app/create-character/create-character.component.spec.ts
+++ rpg-character-builder/src/app/create-character/create-character.component.spec.ts
@@ -20,4 +20,28 @@ describe('CreateCharacterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should generate a random character ID between 1 and 1000 with no decimal places', () => {
+    component.addToCharacter(); // This will trigger the generation of a new order ID
+    expect(component.summary.characterId).toBeGreaterThan(0);
+    expect(component.summary.characterId).toBeLessThanOrEqual(1000);
+    expect(Number.isInteger(component.summary.characterId)).toBe(true);
+  });
+
+  it('should add a character with correct customization', () => {
+    component.selectedCharacterId = 1;
+    component.addToCharacter();
+    const addedCharacter = component.summary.characters[0];
+    expect(addedCharacter.id).toBe(1);
+    expect(addedCharacter.name).toBe("");
+    expect(addedCharacter.gender).toBe("Male");
+    expect(addedCharacter.class).toBe("Warrior");
+  });
+
+
+  it('should reset all form fields to their default values after resetForm is called',() => {
+    component.selectedCharacterId = 2;
+    component.resetForm();
+    expect(component.selectedCharacterId).toBe(1);
+  })
 });
