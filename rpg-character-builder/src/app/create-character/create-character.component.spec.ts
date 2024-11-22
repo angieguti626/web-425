1c8a072a0a4c1c1ad27dabf3ad14b52de11715f9
Angelica Gutierrez <angelicagu368@gmail.com>
2024-11-21 19:18:16 -0800

    fixed issues

--- rpg-character-builder/src/app/create-character/create-character.component.spec.ts
+++ rpg-character-builder/src/app/create-character/create-character.component.spec.ts
@@ -10,7 +10,7 @@ describe('CreateCharacterComponent', () => {
     await TestBed.configureTestingModule({
       imports: [CreateCharacterComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CreateCharacterComponent);
     component = fixture.componentInstance;
@@ -20,9 +20,9 @@ describe('CreateCharacterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
+
   it('should generate a random character ID between 1 and 1000 with no decimal places', () => {
-    component.addToCharacter(); // This will trigger the generation of a new order ID
+    component.generateCharacter(); // This will trigger the generation of a new order ID
     expect(component.summary.characterId).toBeGreaterThan(0);
     expect(component.summary.characterId).toBeLessThanOrEqual(1000);
     expect(Number.isInteger(component.summary.characterId)).toBe(true);
@@ -30,18 +30,32 @@ describe('CreateCharacterComponent', () => {
 
   it('should add a character with correct customization', () => {
     component.selectedCharacterId = 1;
-    component.addToCharacter();
+    component.name = "Thorn";
+    component.gender = "Male";
+    component.class = "Warrior";
+    component.generateCharacter();
+
     const addedCharacter = component.summary.characters[0];
+
     expect(addedCharacter.id).toBe(1);
-    expect(addedCharacter.name).toBe("");
+    expect(addedCharacter.name).toBe("Thorn");
     expect(addedCharacter.gender).toBe("Male");
     expect(addedCharacter.class).toBe("Warrior");
   });
 
 
-  it('should reset all form fields to their default values after resetForm is called',() => {
+  it('should reset all form fields to their default values after resetForm is called', () => {
     component.selectedCharacterId = 2;
+    component.name = "Thorn";
+    component.gender = "Male";
+    component.class = "Warrior";
+
     component.resetForm();
+
     expect(component.selectedCharacterId).toBe(1);
+    expect(component.name).toBe("");
+    expect(component.gender).toBe("");
+    expect(component.class).toBe("");
   })
 });
+
