6552d5e7aeabfe9c004117eb5f551e7546a964de
Angelica Gutierrez <angelicagu368@gmail.com>
2024-11-22 20:24:15 -0800

    signin tests

--- rpg-character-builder/src/app/signin/signin.component.spec.ts
+++ rpg-character-builder/src/app/signin/signin.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { SigninComponent } from './signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
+
+class MockAuthService {
+  signin(email: string, password: string) {
+    return of(true);
+  }
+}
 
 describe('SigninComponent', () => {
   let component: SigninComponent;
@@ -8,10 +17,10 @@ describe('SigninComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SigninComponent]
+      imports: [RouterTestingModule, SigninComponent, ReactiveFormsModule],
+      providers: [{ provide: AuthService, useClass: MockAuthService }]
     })
-    .compileComponents();
-
+      .compileComponents();
     fixture = TestBed.createComponent(SigninComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +28,16 @@ describe('SigninComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call signin method on form submit', () => {
+    // Set up sign in form and assigns values to form controls
+    const signInSpy = spyOn((component as any).authService,'signin').and.callThrough();
+    component.signinForm.controls['email'].setValue('test@example.com');
+    component.signinForm.controls['password'].setValue('Password123');
+    // Calls sign in
+    component.signin();
+    // Checks if values match with assigned
+    expect(signInSpy).toHaveBeenCalledWith('test@example.com','Password123');
   });
 });
