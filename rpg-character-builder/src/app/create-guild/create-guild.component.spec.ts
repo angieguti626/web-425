6674c320f6301c8a103f57b61458be2bafaca48a
Angelica Gutierrez <angelicagu368@gmail.com>
2024-12-07 22:56:45 -0800

    create-guild.component

--- rpg-character-builder/src/app/create-guild/create-guild.component.spec.ts
+++ rpg-character-builder/src/app/create-guild/create-guild.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreateGuildComponent } from './create-guild.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('CreateGuildComponent', () => {
   let component: CreateGuildComponent;
@@ -8,9 +9,9 @@ describe('CreateGuildComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateGuildComponent]
+      imports: [CreateGuildComponent, ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CreateGuildComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,33 @@ describe('CreateGuildComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should be a valid form', () => {
+    component.guildForm.controls['guildName'].setValue('Guild Name');
+    component.guildForm.controls['description'].setValue('Description');
+    component.guildForm.controls['type'].setValue('Competitive');
+    component.guildForm.controls['acceptTerms'].setValue(true);
+    component.guildForm.controls['notificationPreference'].setValue('Email');
+    expect(component.guildForm.valid).toBeTruthy();
+  });
+
+
+  it('should make description required', () => {
+      let control = component.guildForm.get('description');
+      if (control) {
+        control.setValue('');
+        expect(control.valid).toBeFalsy();
+      }
+    });
+
+
+  it('should make guildname required', () => {
+    let control = component.guildForm.get('guildname');
+    if (control) {
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+    }
   });
 });
